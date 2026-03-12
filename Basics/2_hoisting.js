a21d8483dc99a15a5348d365e6f183ef95c7d01f
srikanta0427 <srikanta0427@gmail.com>
2026-03-13 01:04:27 +0530

    Create 2_hoisting.js

---
+++ Basics/2_hoisting.js
@@ -0,0 +1,12 @@
+console.log(x)// gec(global execution context) create a first memory space for this var variable 
+var x = 5; // then assign the value to the variable // thats why it print first undefined
+
+
+//  try this one 
+// console.log(y)
+// let y = 5;
+
+getName()
+function getName(){
+    console.log("print name")
+}
