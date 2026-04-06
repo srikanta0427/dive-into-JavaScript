e5fb3f183e88934a0c7a6f2784d4e65e2fb5c4e2
srikanta0427 <srikanta0427@gmail.com>
2026-04-06 23:11:05 +0530

    Update primose_2.js

--- concurrency/primose_2.js
+++ concurrency/primose_2.js
@@ -1,1 +1,14 @@
 
+const myPromise = new Promise((resolve, reject) => {
+  let success = true;
+
+  if (success) {
+    resolve("Done");
+  } else {
+    reject("Error");
+  }
+});
+
+myPromise
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
